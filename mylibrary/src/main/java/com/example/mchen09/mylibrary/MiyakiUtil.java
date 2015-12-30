ef82aa16bec536d3f1a4628b91ffc612a2626128
miyaki72 <miyakijun@gmail.com>
2015-12-30 16:25:10 +0800

    del app

---
+++ mylibrary/src/main/java/com/example/mchen09/mylibrary/MiyakiUtil.java
@@ -0,0 +1,10 @@
+package com.example.mchen09.mylibrary;
+
+/**
+ * Created by MChen09 on 2015/12/30.
+ */
+public class MiyakiUtil {
+    public static  String getMessage(){
+        return  "miyaki string";
+    }
+}
